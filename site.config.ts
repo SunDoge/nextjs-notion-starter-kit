604647801da78fb5ef3715abeb9950135cdb470a
SunDoge <384813529@qq.com>
2023-04-14 19:11:25 +0800

    update pageid

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '227bac594ed641d497cb47867a4eb73f',
+  rootNotionPageId: '7a5d6ac3992d4a67ace52793c300e35b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'pm-blog-notion.vercel.app',
-  author: 'Me',
+  author: 'Conlin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
